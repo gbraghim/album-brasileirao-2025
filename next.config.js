35b33b67909eb91a10681cd247e46dfaa0b5da47
gbraghim <gbraghim@icloud.com>
2025-04-07 23:39:47 -0300

    Add public directory and update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client']
-  }
+  },
+  output: 'standalone'
 }
 
 module.exports = nextConfig 
