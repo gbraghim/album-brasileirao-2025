23e8c0d1f7df66dcf0c04d633eba2ff3d4db8625
gbraghim <gbraghim@icloud.com>
2025-04-07 23:44:16 -0300

    Add distDir configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   swcMinify: true,
   experimental: {
     serverComponentsExternalPackages: ['@prisma/client']
-  }
+  },
+  distDir: '.next'
 }
 
 module.exports = nextConfig 
