f719bd1e72309fa4596985b7e820d4b744c8553f
gbraghim <gbraghim@icloud.com>
2025-04-23 11:42:39 -0300

    Adiciona suporte para imagens webp e outros formatos no Next.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['s.glbimg.com', 'res.cloudinary.com', 'localhost', 'logodetimes.com', 'api.dicebear.com', 'img.a.transfermarkt.technology'],
+    formats: ['image/webp', 'image/avif'],
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
   env: {
     JWT_SECRET: process.env.JWT_SECRET,
