8b5f4606dcd6e5bfa6b8d0c20c001973a6f92c07
gbraghim <gbraghim@icloud.com>
2025-04-29 20:06:14 -0300

    fix: configura aliases de importação para resolver erros de build

--- next.config.js
+++ next.config.js
@@ -25,6 +25,13 @@ const nextConfig = {
     },
   },
   output: 'standalone',
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
